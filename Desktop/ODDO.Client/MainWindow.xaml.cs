1ca40cc7ca06f287b9138c25c1393611ffd1c14f
Tobias <103662584+TurboThomsen@users.noreply.github.com>
2022-11-29 12:47:18 +0100

    Main Manu - initial commit

--- Desktop/ODDO.Client/MainWindow.xaml.cs
+++ Desktop/ODDO.Client/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,11 @@ namespace ODDODesktop
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
