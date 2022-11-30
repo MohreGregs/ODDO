6974757327d6d576c309ea248a14e88ebb1c989b
Tobias <103662584+TurboThomsen@users.noreply.github.com>
2022-11-30 12:45:09 +0100

    Added Ingridents Table

--- Desktop/ODDO.Client/Views/Ingredients.xaml.cs
+++ Desktop/ODDO.Client/Views/Ingredients.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ODDO.Client.Network;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ODDO.Data.Models;
 
 namespace ODDO.Client.Views
 {
@@ -20,9 +22,98 @@ namespace ODDO.Client.Views
     /// </summary>
     public partial class Ingredients : UserControl
     {
+
+        private List<IngredientModel> data;
+
+        private String sortAttribute;
+
+        bool sortDir = false;
+
         public Ingredients()
         {
             InitializeComponent();
+            getIngredients();
+        }
+
+        private async void getIngredients()
+        {
+            var loadedIncredients = await API.GetIngredient();
+            if (loadedIncredients != null)
+            {
+                this.data = SortIngredients(loadedIncredients);
+                IngredientsList.ItemsSource = this.data;
+            }
+        }
+
+        private List<IngredientModel> SortIngredients(List<IngredientModel> rows)
+        {
+            if (this.sortAttribute == "Name")
+            {
+                if (this.sortDir)
+                {
+                    rows = rows.OrderByDescending(o => o.Name).ToList();
+                } else
+                {
+                    rows = rows.OrderBy(o => o.Name).ToList();
+                }
+            } else if (this.sortAttribute == "Price")
+            {
+                if (this.sortDir)
+                {
+                    rows = rows.OrderByDescending(o => o.Price).ToList();
+                }
+                else
+                {
+                    rows = rows.OrderBy(o => o.Price).ToList();
+                }
+            } else
+            {
+                if (this.sortDir)
+                {
+                    rows = rows.OrderByDescending(o => o.Id).ToList();
+                }
+                else
+                {
+                    rows = rows.OrderBy(o => o.Id).ToList();
+                }
+            }
+            return rows;
+        }
+
+        private void OnSort(object sender, RoutedEventArgs e)
+        {
+            var x = sender as GridViewColumnHeader;
+            if (this.data != null && x != null && x.Name != null)
+            {
+                if (this.sortAttribute == x.Name)
+                {
+                    this.sortDir = !this.sortDir;
+                } else
+                {
+                    this.sortAttribute = x.Name;
+                }
+                IngredientsList.ItemsSource = SortIngredients(this.data);
+            }
+        }
+
+        private void ReloadIngredients(object sender, RoutedEventArgs e)
+        {
+            getIngredients();
+        }
+
+        private void AddIngredients(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Add");
+        }
+
+        private void UpdateIngredient(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Update");
+        }
+
+        private void DeleteIngredient(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Delete");
         }
     }
 }
